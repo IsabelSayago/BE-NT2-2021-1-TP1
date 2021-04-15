0d2aaf2693c72e9355f0cbd031031050ab89e6a3
IsabelSayagoB <40145915+IsabelSayagoB@users.noreply.github.com>
2021-04-15 14:33:50 -0300

    Ejercicio To-do list

--- script.js
+++ script.js
@@ -9,7 +9,56 @@ const classNames = {
   const list = document.getElementById('todo-list')
   const itemCountSpan = document.getElementById('item-count')
   const uncheckedCountSpan = document.getElementById('unchecked-count')
+  const listLi = document.getElementsByTagName('li')
   
-  function addTodo() {
-    alert('Boton Add TODO clickeado!')
-  }
+  let itemCount = 0;
+  let itemUnchecked = 0;
+
+function addTodo() {
+    let todo = prompt("Escriba su to-do")
+    if(addElement(todo)){
+      itemCount++;
+      itemUnchecked++;
+
+      itemCountSpan.innerText = itemCount.toString();
+      uncheckedCountSpan.textContent = itemUnchecked.toString();
+    }  
+}
+
+
+function deleteTodo(){
+  let index = prompt("Ingrese número de to-do");
+
+
+}
+
+
+function addElement(todo){
+    let checkbox = createCheckbox();
+    let liTag = document.createElement('li');
+    if(todo != ""){
+      liTag.textContent = todo;
+
+    liTag.appendChild(checkbox)
+    list.appendChild(liTag);
+    return true;
+    }
+    
+
+}
+
+function createCheckbox(){
+    let checkbox = document.createElement('input')
+    checkbox.type = "checkbox";
+    checkbox.onclick = () => {
+      if(checkbox.checked){
+        itemUnchecked--;
+      }else{
+        itemUnchecked++;
+      }
+      uncheckedCountSpan.textContent = itemUnchecked.toString();
+    } 
+    return checkbox;
+}
+
+const checkboxes = document.getElementsByClassName("check");
